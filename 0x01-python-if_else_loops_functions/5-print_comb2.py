4d9f8c7ecbd0dccdfec3e05d8731654044abf960
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-10 21:33:36 +0000

    Changed to exactly 2 prints and pep8 fixes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number < 10:
-        print("0{:d}".format(number), end = ", ")
-    elif number == 99:
-        print("{:d}".format(number))
+    if number < 99:
+        print("{:02d}".format(number), end=", ")
     else:
-        print("{:d}".format(number), end = ", ")
+        print("{:d}".format(number))
