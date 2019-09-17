a5ace2176ebb4119dea091a1ffbc5752321fa763
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-17 22:22:21 +0000

    Changed print syntax

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     limit = len(my_list)
     if my_list:
-        for numbers in reversed(range(0, limit)):
-            print("{:d}".format(my_list[numbers]))
+        for numbers in reversed(my_list):
+            print("{:d}".format(numbers))
