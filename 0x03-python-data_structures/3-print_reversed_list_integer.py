0ca40d77f9ba1462c90389f5944f428a2d9c983d
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-17 20:58:08 +0000

    Added condition where list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     limit = len(my_list)
+    if not my_list:
+        return None
     for numbers in reversed(range(0, limit)):
         print("{:d}".format(my_list[numbers]))
