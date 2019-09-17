b2ff01f47644a92629b4b45f7e3d3c55cecc98f4
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-17 22:19:47 +0000

    Removed return None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     limit = len(my_list)
-    if not my_list:
-        return None
-    for numbers in reversed(range(0, limit)):
-        print("{:d}".format(my_list[numbers]))
+    if my_list:
+        for numbers in reversed(range(0, limit)):
+            print("{:d}".format(my_list[numbers]))
