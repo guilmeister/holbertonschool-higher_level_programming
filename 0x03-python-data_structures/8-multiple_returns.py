fe7a0d506f1b9b12903b04187eb8792f3563a1ed
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-18 04:37:04 +0000

    Changed to return None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if sentence is not None:
-        return (length, sentence[0])
+        return (length, None)
     return (length, sentence[0])
