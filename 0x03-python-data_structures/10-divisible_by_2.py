ed00890bf743f9fa70f62a0bd6b49d1e79764d94
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-18 01:58:42 +0000

    Changed syntax to append

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list:
-        new_list = list(my_list)
-        for numbers in new_list:
-            if numbers % 2 == 0:
-                new_list[numbers] = True
-            else:
-                new_list[numbers] = False
-        return new_list
+    new_list = []
+    for numbers in new_list:
+        if numbers % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
