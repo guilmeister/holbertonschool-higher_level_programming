31b5e802229dbe1c5c0edb00f4169223a675253a
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-18 01:51:35 +0000

    Added if empty list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = list(my_list)
-    for numbers in new_list:
-        if numbers % 2 == 0:
-            new_list[numbers] = True
-        else:
-            new_list[numbers] = False
-    return new_list
+    if my_list:
+        new_list = list(my_list)
+        for numbers in new_list:
+            if numbers % 2 == 0:
+                new_list[numbers] = True
+            else:
+                new_list[numbers] = False
+        return new_list
