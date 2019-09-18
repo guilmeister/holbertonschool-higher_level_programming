3521a4958242be206d4cd6412f77141b2e63bec0
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-18 03:03:53 +0000

    Added empty case and none case

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is None:
+    if my_list is None or my_list is []:
         return None
     new_list = my_list.copy()
     for numbers in new_list:
