7d3d6818dbb31af4133edc406c769c7717768654
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-18 02:47:12 +0000

    Added 0 length return on empty sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return None
+        return (0, None)
     length = len(sentence)
     first = ''
     for char in sentence:
