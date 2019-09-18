d37e2fd70f587ab8b24945166ab15c1c10308576
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-18 01:34:14 +0000

    Added python function that finds multiples of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    new_list = list(my_list)
+    for numbers in new_list:
+        if numbers % 2 == 0:
+            new_list[numbers] = True
+        else:
+            new_list[numbers] = False
+    return new_list
