61ff1bfde7c61a783267fa2f22b6014bf8007ef3
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-18 03:18:57 +0000

    looping on wrong list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if my_list is None or my_list is []:
         return None
     new_list = []
-    for numbers in new_list:
+    for numbers in my_list:
         if numbers % 2 == 0:
             new_list.append(True)
         else:
