e8963ad71d669a8ae548aee20b136427ed8f109d
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-19 20:47:06 +0000

    pep8 fixes

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for x, y in sorted(a_dictionary.items()):
-        print ("{} : {}".format(x, y))
+        print("{} : {}".format(x, y))
