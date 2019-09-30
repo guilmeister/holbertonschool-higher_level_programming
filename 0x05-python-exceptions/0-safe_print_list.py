7402e5a4ff43b64b245c48d15aa94e7588062d6f
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-30 21:06:54 +0000

    Changed syntax to loop outside and moved try/except inside loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for y in range(0, x):
+    counter = 0
+    for y in range(0, x):
+        try:
             print("{:d}".format(my_list[y]), end="")
-        print("")
-        return(y + 1)
-    except IndexError:
-        print("")
-        return(y)
+            counter = counter + 1
+        except:
+            continue
+    print("")
+    return(counter)
