0844acfa6cd4f3978541a39101753dbb85bc1fc4
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-30 19:38:22 +0000

    removed unused variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    length = 0
-    for i in my_list:
-        length = length + 1
     try:
         for y in range(0, x):
             print("{:d}".format(my_list[y]), end="")
