811dfb415fe7c344e5efd3ce7a26d1a66ad03d69
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-30 19:14:51 +0000

    Changed to check all exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[y]), end="")
         print("")
         return(y + 1)
-    except IndexError:
+    except:
         print("")
         return(y)
