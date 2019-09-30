a5ac7ce3d658c9a21db2f194c7ee7962f1eb1279
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-30 17:01:09 +0000

    Added python function that prints elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    length = 0
+    for i in my_list:
+        length = length + 1
+    try:
+        for y in range(0, x):
+            print("{}".format(my_list[y]), end="")
+        print("")
+        return(y + 1)
+    except IndexError:
+        print("")
+        return(y)
