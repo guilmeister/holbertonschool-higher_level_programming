f96d70ad937fd65be89121e350fd5b2362870921
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-30 19:18:26 +0000

    Changed print syntax

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         length = length + 1
     try:
         for y in range(0, x):
-            print("{}".format(my_list[y]), end="")
+            print("{:d}".format(my_list[y]), end="")
         print("")
         return(y + 1)
     except:
