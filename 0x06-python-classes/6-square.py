d4b2509d5fbf60ea7c0c235da42ca933fe58294f
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-01 23:56:53 +0000

    Added more edgecase checks on tuples

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,13 @@ class Square:
         """Getter for position"""
         if isinstance(value, tuple) is False:
             raise TypeError("position must be a tuple of 2 positive integer")
-        if value[0] < 0 and value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        if isinstance(value[0], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        if isinstance(value[1], int) is False:
             raise TypeError("position must be a tuple of 2 positive integer")
         self.__position = value
 
