15762d748b6771500716571ad7c9805dbf9d7727
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-01 23:01:29 +0000

    Added square class

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializer"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Getter for position"""
+        if position.index(0) < 0 or position.index(1) < 0:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
+
+    def my_print(self):
+        """Printing box"""
+        if self.__size == 0:
+            print("")
+        for x in range(0, self.__position[1]):
+            print("")
+        for x in range(0, self.__size):
+            for z in range(0, self.__position[0]):
+                print(" ", end="")
+            for y in range(0, self.__size):
+                print("#", end="")
+            print("")
+
+    def area(self):
+        """Returning size"""
+        return self.__size
