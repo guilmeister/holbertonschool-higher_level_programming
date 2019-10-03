d0e2f73a28711b370b60f232a83d2e8c5d11a035
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-03 20:56:15 +0000

    Reminder to fix test case where size of rows is not equal

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    for outer in range(0, len(matrix)):
+        for inner in range(0, len(matrix[outer])):
+            if inner > len(matrix[outer]):
+                raise TypeError("Each row of the matrix must have the same size")
+            if isinstance(matrix[outer][inner], (int, float)) is False:
+                raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+    if isinstance(div, (int, float)) is False:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    field_reset = []
+    new_matrix = []
+    for outer in range(0, len(matrix)):
+        field_reset = []
+        for inner in range(0, len(matrix[outer])):
+            quotient = round(matrix[outer][inner] / div, 2)
+            field_reset.append(quotient)
+        new_matrix.append(field_reset)
+    return new_matrix
