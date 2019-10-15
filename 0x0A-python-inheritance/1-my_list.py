9e9e920afa3847accfe9811d05ee2831caff5eae
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-15 22:28:41 +0000

    added function that prints list that is sorted

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class MyList(list):
+    """
+    Class MyList that takes in list
+    """
+    def print_sorted(self):
+        """
+        Print sorted list
+        """
+        print(sorted(self))
