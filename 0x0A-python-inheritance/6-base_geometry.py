339e5ceceee58edf756d0974135d7027742b7582
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-15 21:24:30 +0000

    Added comments on functions and classes

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 class BaseGeometry:
+    """
+    Empty Class BaseGeometry
+    """
     pass
 
     def area(self):
+        """
+        Function that prints the area
+        """
         raise Exception("area() is not implemented")
