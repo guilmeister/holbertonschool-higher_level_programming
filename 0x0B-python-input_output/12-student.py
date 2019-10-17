f2dd1be40ab4129a12c432a298eb4e79af1bf515
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-17 01:46:24 +0000

    added function that defines student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        filtered = {}
+        if attrs == None:
+            return self.__dict__
+        for key in attrs:
+            if key in self.__dict__.keys():
+                filtered[key] = self.__dict__[key]
+        return filtered
