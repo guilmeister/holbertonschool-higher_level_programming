bae305ce38292bd499fca1c672b6111233186adb
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-22 19:50:06 +0000

    pep8 fixes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import json
 
+
 class Base:
 
     __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
-#            Base.__nb_objects = Base.__nb_objects + 1
             self.id = id
         else:
             Base.__nb_objects = Base.__nb_objects + 1
