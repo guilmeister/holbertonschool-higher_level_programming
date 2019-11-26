7e34f8c04d5951f5ed7c398e278e3ca8fafc6fc6
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-26 22:00:51 +0000

    added files

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Comments:
+-- Script that lists all databases of your MySQL server
+SHOW GRANTS FOR 'user_0d_1';
+SHOW GRANTS FOR 'user_0d_2';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Comments:
+-- Script that lists all databases of your MySQL server
+CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1;
