63db7dd8dd4d3772ea9530daa38a362c044c29d3
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-26 22:02:02 +0000

    syntax change

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Comments:
 -- Script that lists all databases of your MySQL server
-SHOW GRANTS FOR 'user_0d_1';
-SHOW GRANTS FOR 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
