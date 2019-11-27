1f2d16399613dde991ca455cb154e8c427b645c4
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-27 05:20:37 +0000

    added 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Comments:
+-- Script that lists all databases of your MySQL server
+SELECT tv_genres.name AS name FROM tv_show_genres
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name ASC;
