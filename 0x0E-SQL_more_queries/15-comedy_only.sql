0dec614136c65e8e8f02cb695c65f0b2a628c895
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-27 05:33:43 +0000

    made AS capital

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Comments:
 -- Script that lists all databases of your MySQL server
-SELECT tv_shows.title as title FROM tv_shows
+SELECT tv_shows.title AS title FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
