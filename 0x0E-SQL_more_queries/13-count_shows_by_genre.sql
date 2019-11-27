1dbdd7f6eb877e158467e4f18a5141a63711a98f
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-27 02:51:43 +0000

    changed order

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Comments:
 -- Script that lists all databases of your MySQL server
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres
-LEFT JOIN tv_show_genres
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_show_genres
+LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
