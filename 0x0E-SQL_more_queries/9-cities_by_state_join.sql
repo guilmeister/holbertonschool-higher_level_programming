85670423ac0c95b9fa5dcf97fa4823bdf601f9a1
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-27 01:19:39 +0000

    changed 9 removed join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Comments:
 -- Script that lists all databases of your MySQL server
 SELECT cities.id AS id, cities.name AS name, states.name AS name
-FROM hbtn_0d_usa.states
-INNER JOIN cities
+FROM states, cities
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
