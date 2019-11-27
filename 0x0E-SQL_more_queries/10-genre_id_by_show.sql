c8344e61677b2dedf74dae1a41ad5594bfde60cc
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-27 01:29:36 +0000

    added 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Comments:
+-- Script that lists all databases of your MySQL server
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_shows
+INNER JOIN tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
