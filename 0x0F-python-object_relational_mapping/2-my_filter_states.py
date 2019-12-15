baafc5269aa5e44456fa8181a6f1d860e8f3da05
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-15 22:27:46 +0000

    added BINARY to check if exactly the same

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,9 @@ if __name__ == "__main__":
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states " +
+                "WHERE BINARY name = '{}' ".format(sys.argv[4]) +
+                "ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
