74690347fc44149077f4940a86126b2316359670
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-16 00:04:25 +0000

    added argv[4] as parameter, specified type and value of argv[4], pep8 fixes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,8 +18,7 @@ if __name__ == "__main__":
                            charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states " +
-                "WHERE BINARY name = '{}' ".format(sys.argv[4]) +
-                "ORDER BY id ASC")
+                "WHERE BINARY name = %s", (sys.argv[4], ))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
