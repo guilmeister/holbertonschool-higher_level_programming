0c4721e848f1a2b851af0b9fbf2c79cf62645d78
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-16 05:49:14 +0000

    pep8 fixes

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name = "Louisiana")
+    new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
     for state in session.query(State).order_by(State.id).all():
