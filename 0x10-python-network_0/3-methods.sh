fe2bd8c0e7c207c2f3a095292a03ca7b3ae6638c
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-07 00:38:22 +0000

    added complement, cut field 1 instead of 2, made more dynamic

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes in a URL and displays all HTTP method server will accept
-curl -sI "$1" | grep Allow | cut -d' ' -f2-4
+curl -sI "$1" | grep Allow | cut --complement -d' ' -f1
