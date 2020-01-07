f86657cca2e09088019c57ed910b6a2ffcbafea9
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-07 01:11:06 +0000

    wrong output, changed semi-colon to equal

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes in a URL as an argument, sends a GET request to the URL
-curl --silent POST --data "email: hr@holbertonschool.com" --data "subject: I will always be here for PLD" "$1"
+curl --silent POST --data "email=hr@holbertonschool.com" --data "subject=I will always be here for PLD" "$1"
