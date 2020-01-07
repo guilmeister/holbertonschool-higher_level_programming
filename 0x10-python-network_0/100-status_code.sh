b6c730e9be3e2950330171184856809577bc548d
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-07 03:57:21 +0000

    removed random new line, no more extra line, made flags explicit

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that sends a request to a URL passed as an argument
-curl -s -o /dev/null -I -w "%{http_code}\n" "$1"
+curl --silent --output /dev/null --head --write-out "%{http_code}" "$1"
