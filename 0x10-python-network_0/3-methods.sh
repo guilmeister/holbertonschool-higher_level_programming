7184d49fe827f471f509cc371cfc9ebd87a80cc1
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-07 00:40:39 +0000

    try

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes in a URL and displays all HTTP method server will accept
-curl -sI "$1" | grep Allow | cut --complement -d' ' -f1
+curl --silent --head "$1" | grep Allow | cut --complement -d' ' -f1
