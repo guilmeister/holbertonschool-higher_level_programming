ba0b12a6900cecd7c1eafc737ec2ae4b2aba40c3
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-09 05:05:01 +0000

    added 102

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Script that takes in a URL, request and displays size of body response
+curl --silent --location --header 'Origin: HolbertonSchool' --request PUT --data 'user_id=98' 0.0.0.0:5000/catch_me
