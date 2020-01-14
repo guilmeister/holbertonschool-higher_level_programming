212167b2b48a1395c6b2233999212d6f178dfea1
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-14 05:37:50 +0000

    added 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        a_dict = {'q': ''}
+    elif len(sys.argv) > 1:
+        a_dict = {'q': sys.argv[1]}
+    req = requests.post('http://0.0.0.0:5000/search_user', a_dict)
+    if 'json' not in req.headers.get('content-type'):
+        print('Not a valid JSON')
+    else:
+        if req.json():
+            print('[{}] {}'.format(req.json().get('id'), req.json().get('name')))
+        else:
+            print('No result')
