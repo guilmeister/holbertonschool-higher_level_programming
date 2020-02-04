4ad51a4a9e72b0bba709beb45c6118261641ef22
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-04 21:04:05 +0000

    semistandard fixes

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 if (process.argv[2] === undefined || isNaN(Number(process.argv[2]))) {
   console.log('Missing size');
 } else {
-  let string = 'X';
+  const string = 'X';
   let x = 0;
   for (; x < parseInt(process.argv[2]); x++) {
     console.log(string.repeat(process.argv[2]));
-    }
+  }
 }
