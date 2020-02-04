8f6d60258584b8471b4254caf4e9b399b2f34d8e
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-04 21:27:22 +0000

    added task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function factorial (number) {
+  if (isNaN(number)) {
+    return (1);
+  } else if (number < 0) {
+    return (-1);
+  } else if (number === 0) {
+    return (1);
+  } else {
+    return (number * factorial(number - 1));
+  }
+}
+console.log(factorial(process.argv[2]));
