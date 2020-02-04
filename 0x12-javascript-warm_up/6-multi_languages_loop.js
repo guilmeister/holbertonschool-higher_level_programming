ddeece0085fabf05bb0bea31fb339df707bc6bf7
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-04 20:32:07 +0000

    added task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const languageArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let x = 0;
+for (; x < languageArray.length; x++) {
+  console.log(languageArray[x]);
+}
