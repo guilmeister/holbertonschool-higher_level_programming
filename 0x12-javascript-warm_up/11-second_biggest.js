bc3b0f25a18646bc256b99578df57b20ad9ab12d
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-04 22:21:55 +0000

    added task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+function findSecondMax (array) {
+  const arr = array.splice(2).sort();
+  const max = Math.max.apply(null, arr);
+  arr.splice(arr.indexOf(max), 1);
+  return Math.max.apply(null, arr);
+}
+if (process.argv[2] === undefined || process.argv.length === 3) {
+  console.log('0');
+  process.exit();
+}
+const arrays = [];
+let x = 0;
+for (; x < process.argv.length; x++) {
+  arrays.push(process.argv[x]);
+}
+console.log(findSecondMax(arrays));
