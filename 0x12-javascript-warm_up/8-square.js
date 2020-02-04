28db68d693419cc5130fe53cd93697bec52c7524
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-04 21:02:51 +0000

    added task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(Number(process.argv[2]))) {
+  console.log('Missing size');
+} else {
+  let string = 'X';
+  let x = 0;
+  for (; x < parseInt(process.argv[2]); x++) {
+    console.log(string.repeat(process.argv[2]));
+    }
+}
