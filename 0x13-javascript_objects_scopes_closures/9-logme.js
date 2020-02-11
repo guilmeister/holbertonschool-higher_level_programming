106612350183322e8fe94985e4ec9c520d6c580f
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-11 05:20:42 +0000

    added task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+let counter = 0;
+
+exports.logMe = function (item) {
+  const logList = [];
+  let x = logList.length;
+  console.log(logList.length);
+  logList.push(item);
+  for (; x < logList.length; x++) {
+    if (logList[x] === item) {
+      console.log(counter + ': ' + logList[x]);
+    }
+  }
+  counter++;
+};
