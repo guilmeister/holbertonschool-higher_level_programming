bee388851a02380a5b0e6554f8b4cc718ec82d65
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-11 04:30:26 +0000

    followed task requirement, inherits from class Square of file 5-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,40 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w === 0 || w < 0 || h === 0 || h < 0 || isNaN(w) || isNaN(h)) {
-
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    const string = 'X';
-    let x = 0;
-    for (; x < this.height; x++) {
-      console.log(string.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-const fiveSquareDotJs = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-};
+const fiveSquareDotJs = require('./5-square');
 
 class Square extends fiveSquareDotJs {
   charPrint (c) {
@@ -48,5 +14,4 @@ class Square extends fiveSquareDotJs {
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;
