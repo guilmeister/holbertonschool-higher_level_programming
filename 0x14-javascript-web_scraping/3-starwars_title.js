fb68660469c61f6a765f4264ae1d3af0d19ca2ac
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-11 20:34:41 +0000

    added task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const search = process.argv[2];
+const url = 'http://swapi.co/api/films/' + search;
+console.log(url);
+const request = require('request');
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const title = JSON.parse(body).title;
+  console.log(title);
+});
