3044e0a73d60cc89365d3d9076e095d365008c8e
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-11 03:32:48 +0000

    added task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w === 0 || w < 0 || h === 0 || h < 0 || isNaN(w) || isNaN(h)) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const string = 'X';
+    let x = 0;
+    for (; x < this.height; x++) {
+      console.log(string.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
