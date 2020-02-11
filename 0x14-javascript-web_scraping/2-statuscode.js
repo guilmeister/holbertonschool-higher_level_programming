2fed7f0758f253bdc53c2c78771365ceff5db253
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-11 20:02:21 +0000

    semistandard fix, wants error to be handled

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,9 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, function (error, response) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log('code: ' + response.statusCode);
 });
